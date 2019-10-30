f7be10e1932dd22c72c6548ef6f833f9c7dca07c
mssreerag <sreeragms@cet.ac.in>
2019-10-30 22:34:37 +0530

    Addedd time to homepage New hover effect Favicon added Added nightmode, autimatic according to time Included toggle for nightmode

---
+++ script.js
@@ -0,0 +1,79 @@
+var clockElement=document.getElementById("clock");
+let minuets,hour=0,flag=0,flag2=0;
+var back=document.getElementById("fullBack");
+var search=document.getElementById("searchBox");
+
+document.getElementById("searchBox").focus();
+setInterval(function(){updateClock(clockElement);},100);
+
+mode();
+
+function updateClock(clock){
+    var timeSec=new Date().toLocaleTimeString();
+    var time=timeSec.split(':')
+    hour=parseInt(time[0]);
+    minuets=parseInt(time[1]);
+    clock.innerHTML=hour+':'+minuets;
+    if(flag2==0)
+    {mode();
+        flag2=1;}
+}
+function mode(){
+
+if(hour>19)
+{
+    console.log();
+    night();
+    flag=1;
+    
+}
+    else
+{
+    // console.log(hour);
+
+    day();
+    flag=0;
+}
+
+}
+function dayNight(){
+    flag++;
+    if(flag%2==1)
+    {
+        night();
+    }
+    else{
+        day();
+    }
+}
+
+
+
+
+
+function night(){
+    back.style.backgroundColor="#666666";
+    search.style.backgroundColor="#555555";
+    search.style.color="#bbbbbb";
+    search.style.borderBottom="none"
+    clockElement.style.color="#bfbbbb";
+    // document.getElementById("toggleBtn").style.outline="none";
+    // document.getElementById("toggleBtn").style.border="none";
+    document.getElementById("toggleMoon").style.display="none";
+    document.getElementById("toggleSun").style.display="inline";
+
+}
+function day(){
+    back.style.backgroundColor="#ffffff";
+    search.style.backgroundColor="#ffffff";
+    clockElement.style.color="#555555";
+    search.style.borderBottom="solid 2px #aa2211"
+    clockElement.style.color="#555555";
+    document.getElementById("toggleSun").style.display="none";
+    document.getElementById("toggleMoon").style.display="inline";
+
+
+
+
+
+}
